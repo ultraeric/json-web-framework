6bbbc8dcff8ffa4b57b4931909a673a115f65b67
ultraeric <ericemily@MacBook-Pro.local>
2016-04-29 11:21:36 -0700

    Added GPL LGPL to HttpOptions

--- src/main/java/client/HttpOptions.java
+++ src/main/java/client/HttpOptions.java
@@ -1,3 +1,20 @@
+/*   
+  	This file is part of JSON Web Framework.
+
+    JSON Web Framework is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    JSON Web Framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with JSON Web Framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package client;
 
 import java.util.HashMap;
